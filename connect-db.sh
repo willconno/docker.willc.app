9f01890b66ee6d880700c3002e034587e3d7b32a
William Connelly <connelly.william@gmail.com>
2020-10-15 22:39:13 +0800

    initial commit

---
+++ connect-db.sh
@@ -0,0 +1,1 @@
+docker-compose exec db sh -c "$(cat ./database/connect.sh)"
---
+++ database/create-user.sql
@@ -0,0 +1,3 @@
+create user 'root'@'%' identified by 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+FLUSH PRIVILEGES;
